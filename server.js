870a61237de0e24cb0598d925228d3fb43fa5d32
Shafiya Ariff <shafiya.ariff23@gmail.com>
2020-12-16 00:09:30 +0530

    Add Authentication Flow

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(bodyParser.urlencoded({
 
 app.get('/',(req,res) => res.send('API Running'));
 
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
